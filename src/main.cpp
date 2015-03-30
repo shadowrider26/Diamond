46627e7c0d688af2a1e9a07c09c4d5c9c00948da
Eagle Flies <myeagleflies@gmail.com>
2015-03-30 20:53:54 +0200

    Testing - setting switch to 1000000

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ bool eagleoldclients = false;
 int64 eaglesubsidy = 0;
 int eagledecreased = 0;
 int eaglespacing = 0;
-int64 FIRST_REWARD_DECREASE_AT_COIN = 971510;
+int64 FIRST_REWARD_DECREASE_AT_COIN = 1000000;
 int64 SECOND_REWARD_DECREASE_AT_COIN = 2500000;
 
 
