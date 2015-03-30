ba026557f0fc76273c03a8fd6d7559cf27817d32
Eagle Flies <myeagleflies@gmail.com>
2015-03-30 21:14:07 +0200

    Testing switch to happen on independent network: port 37771. 45 blocks in the future

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 27771 : 17771;
+    return testnet ? 27771 : 37771;
 }
 
 
